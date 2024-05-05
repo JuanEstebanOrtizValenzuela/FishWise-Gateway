ad3106decc8f167ef7c8bf59dc50d2ecb7f21704
JuanOrtiz <juan_ortizva@fet.edu.co>
2024-05-05 15:31:58 -0500

    dev env

---
+++ src/main/java/com/FishWise/Gateway/FishWiseGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.FishWise.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishWiseGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishWiseGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FishWise/Gateway/FishWiseGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FishWise.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishWiseGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
